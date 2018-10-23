55b09ef759e27880eb49797676a072f5c802ad52
micky04 <41420072+micky04@users.noreply.github.com>
2018-10-23 10:03:28 +0800

    Add files via upload

---
+++ predict_reply.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+greeting=(["hi","hey","hello"])
+def classify(msg):
+    return "Hi"
+
+if __name__ == '__main__':
+    while(1):
+      msg=raw_input("Enter something: ")
+      print(classify(msg))	
