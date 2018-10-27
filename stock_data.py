ded9da2b2dec7433355fc8c7e0a21b295f35bbbd
micky04 <41420072+micky04@users.noreply.github.com>
2018-10-27 15:47:11 +0800

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -11,6 +11,7 @@ def stock_match(msg):
         start()
     data = json.load(original)
 
+    ## Shows the most likely stock the user is searching for
     best_match = [data['bestMatches'][0]['1. symbol'],data['bestMatches'][0]['9. matchScore']]
     return best_match
 
@@ -23,13 +24,22 @@ def get_data(sym,request_type):
     data = json.load(original)
     if request_type == 'chart/dynamic':
         data = data['data']
+    ## Returns the stock data requested by other functions
     return data
 
 def stock_info(stock):
-    global data_company
     data = get_data(stock,'book')
     data_company = get_data(stock,'company')
     website = data_company['website']
+
+    ## Shows the stock's latest price, the time of quote, and website url
     info = [data['quote']['close'],data['quote']['latestTime'],website.replace('http://www.','')]
     return info
+
+def stock_describe(stock):
+    data = get_data(stock,'company')
+
+    ## Shows the stock's name, CEO, sector, industry and description
+    info = [data['companyName'],data['website'],'CEO',data['CEO'],'Sector',data['sector'],'Industry',data['industry'],'Description',data['description']]
+    return info 
     
