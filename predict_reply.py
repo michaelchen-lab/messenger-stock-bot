ee7bbc47c8577f5f0b87014a5b097977ec99a7cf
micky04 <41420072+micky04@users.noreply.github.com>
2018-10-27 15:54:14 +0800

    Update predict_reply.py

--- predict_reply.py
+++ predict_reply.py
@@ -6,19 +6,34 @@ import stock_data as sd
 greeting=(["What's up!","Hey, nice to meet you!","Hi, it's good to hear from you again!"])
 def classify(msg):
     
-    ##When msg is just one word, we assume the user sent a company name or symbol
     if " " not in msg:
+        ## when the user enters a company name or ticker symbol
         match = best_match(msg)
         if match == []:
-            return "Sorry, I don't understand you."
-        info = sd.stock_info(match[0])
+            return "Sorry, I don't understand you.","",""
+        info = stock_info(match[0])
         print(info)
-        return "$"+str(info[0])+" (as of "+info[1]+")",match[0],info[2]       
+        return "$"+str(info[0])+" (as of "+info[1]+")",match[0],info[2],'symbol'
+    
+    elif "description" in msg or "Description" in msg:
+        ## when the user asks for the description of a company
+        try:
+            msg = msg.replace('description','')
+        except:
+            msg = msg.replace('Description','')
+        msg = msg.strip()
+        match = best_match(msg)
+        return stock_describe(match[0]),'','','list'
 
     return "Sorry, I don't understand you.","",""
 
+## Link to stock_data.py
 def best_match(msg):
     return sd.stock_match(msg)
+def stock_info(stock):
+    return sd.stock_info(stock)
+def stock_describe(stock):
+    return sd.stock_describe(stock)
 
 if __name__ == '__main__':
     while(1):
