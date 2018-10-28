7626cc11383441d3050daf84060dc19d1af7f9da
micky04 <41420072+micky04@users.noreply.github.com>
2018-10-28 23:30:47 +0800

    Update predict_reply.py

--- predict_reply.py
+++ predict_reply.py
@@ -10,9 +10,9 @@ def classify(msg):
         ## when the user enters a company name or ticker symbol
         match = best_match(msg)
         if match == []:
-            return "Sorry, I don't understand you.","",""
+            return "Sorry, I don't understand you.","","","other",1
         info = stock_info(match[0])
-        return "$"+str(info[0])+" (as of "+info[1]+")",match[0],info[2],'symbol'
+        return "$"+str(info[0])+" (as of "+info[1]+")",match[0],info[2],"symbol",1
     
     elif "description" in msg or "Description" in msg:
         ## when the user asks for the description of a company
@@ -20,11 +20,20 @@ def classify(msg):
             msg = msg.replace('description','')
         except:
             msg = msg.replace('Description','')
-        msg = msg.strip()
-        match = best_match(msg)
-        return stock_describe(match[0]),'','','list'
+        match = best_match(msg.strip())
+        return stock_describe(match[0]),'','','list',1
 
-    return "Sorry, I don't understand you.","",""
+    elif "income" in msg or "Income" in msg:
+        ## when the user asks for the income statement of a company
+        try:
+            msg = msg.replace('income','')
+        except:
+            msg = msg.replace('Income','')
+        match = best_match(msg.strip())
+        info,info2 = stock_income(match[0])
+        return info,info2,'','list',2
+
+    return "Sorry, I don't understand you.","","","other",1
 
 ## Link to stock_data.py
 def best_match(msg):
@@ -33,6 +42,8 @@ def stock_info(stock):
     return sd.stock_info(stock)
 def stock_describe(stock):
     return sd.stock_describe(stock)
+def stock_income(stock):
+    return sd.stock_income(stock)
 
 if __name__ == '__main__':
     while(1):
