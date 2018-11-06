b47bdc4f942aea48032cdc2b2ac88e25c7346fb8
micky04 <41420072+micky04@users.noreply.github.com>
2018-11-06 09:45:22 +0800

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -4,19 +4,24 @@ import urllib.request
 import json
 
 def stock_match(msg):
+    ## Uses the API's search feature to return the most likely stock the user is searching for
+    
     try:
+        ## Uses the ALPHAVANTAGE API
         original = urllib.request.urlopen('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords='+msg+'&apikey=3VWEA280OOGR9AL4')
     except ValueError:
         print("\nInvalid Entry")
         start()
     data = json.load(original)
 
-    ## Shows the most likely stock the user is searching for
     best_match = [data['bestMatches'][0]['1. symbol'],data['bestMatches'][0]['9. matchScore']]
     return best_match
 
 def get_data(sym,request_type):
+    ## Returns the stock data requested by other functions
+    
     try:
+        ## Uses the IEX API
         original = urllib.request.urlopen('https://api.iextrading.com/1.0/stock/'+sym+'/'+request_type)
     except ValueError:
         print("\nInvalid Entry")
@@ -24,7 +29,6 @@ def get_data(sym,request_type):
     data = json.load(original)
     if request_type == 'chart/dynamic':
         data = data['data']
-    ## Returns the stock data requested by other functions
     return data
 
 def num(num):
@@ -36,6 +40,8 @@ def num(num):
     return num
 
 def stock_info(stock):
+    ## Returns general info about stock (the most basic feature)
+    
     data = get_data(stock,'book')
     data_company = get_data(stock,'company')
     website = data_company['website']
@@ -46,7 +52,8 @@ def stock_info(stock):
     return info
 
 def stock_describe(stock):
-    global info2
+    ## Returns info for stock description
+    
     data = get_data(stock,'company')
     website = data['website']+'/'
     try:
@@ -65,8 +72,21 @@ def stock_describe(stock):
     return info,info2
 
 def stock_income(stock):
+    ## Return info for stock income statement
+    
+    data = get_data(stock,'financials?period=annual')
+    data2 = get_data(stock,'company')
+
+    info = [data2['companyName'],'https://seekingalpha.com/symbol/'+stock+'/income-statement','Operating Revenue',num(data['financials'][0]['operatingRevenue']),'Gross Profit',num(data['financials'][0]['grossProfit']),'Operating Income',num(data['financials'][0]['operatingIncome'])]
+    info2 = ['Operating Expense',num(data['financials'][0]['operatingExpense']),'Net Income',num(data['financials'][0]['netIncome']),"R&D",num(data['financials'][0]['researchAndDevelopment']),'Report Date',data['financials'][0]['reportDate']] 
+    
+    return info,info2
+
+def stock_balance(stock):
+    ## Return the info for stock balance sheet
+
     global data
-    global info
+
     data = get_data(stock,'financials?period=annual')
     data2 = get_data(stock,'company')
     website = data2['website']+'/'
@@ -75,8 +95,7 @@ def stock_income(stock):
     except:
         pass
 
-    info = [data2['companyName'],website,'Operating Revenue',num(data['financials'][0]['operatingRevenue']),'Gross Profit',num(data['financials'][0]['grossProfit']),'Operating Income',num(data['financials'][0]['operatingIncome'])]
-    info2 = ['Operating Expense',num(data['financials'][0]['operatingExpense']),'Net Income',num(data['financials'][0]['netIncome']),"R&D",num(data['financials'][0]['researchAndDevelopment']),'Report Date',data['financials'][0]['reportDate']]
-    
-    return info,info2
-    
+    info = [data2['companyName'],'https://seekingalpha.com/symbol/'+stock+'/balance-sheet','Cash Flow',num(data['financials'][0]['cashFlow']),'Shareholder Equity',num(data['financials'][0]['shareholderEquity']),'Total Assets',num(data['financials'][0]['totalAssets'])]
+    info2 = ['Total Cash',num(data['financials'][0]['totalCash']),'Current Debt',num(data['financials'][0]['currentDebt']),'Total Debt',num(data['financials'][0]['totalDebt']),'Report Date',data['financials'][0]['reportDate']]    
+
+    return info,info2    
